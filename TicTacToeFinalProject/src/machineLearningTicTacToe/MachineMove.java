43ba743f1d5be681635ec7f7b12284c5373c304a
moynihanm1 <moynihanm1@BY1928A75002102.wit.private>
2019-11-14 17:52:31 -0500

    Finished MachineMove and added Comments

--- TicTacToeFinalProject/src/machineLearningTicTacToe/MachineMove.java
+++ TicTacToeFinalProject/src/machineLearningTicTacToe/MachineMove.java
@@ -1,19 +1,37 @@
 package machineLearningTicTacToe;
 
 public class MachineMove implements iPlayer {
-	//private int turn = 0;
-	
+	//int to represent which player the machine is playing for.
+	private int turn = 0;
+	//local copy of the current gameState updated each 'play()'
+	private int[] moveIndex = {0,0,0,0,0,0,0,0,0};
+	//instantiates a Machine to be used 
+	private Machine mech = new Machine();
+	/**
+	 * Constructor passed which player (x or o),(2, 1) it will be playing for.
+	 * @param turn
+	 */
 	public MachineMove(int turn) {
-		//this.turn = turn;
+		if(turn == 1 || turn == 2) {
+			this.turn = turn;
+		} else {
+			System.out.println("Error: Incorrect MachineMove usage");
+		}
 	}
+	
 	@Override
 	public void setMove(int move) {
-		//Game.setGameState(n);
-
+		int[] temp = Game.getGameState();
+		temp[move] = this.turn;
+		Game.setGameState(temp);
 	}
-
+	
+	/**
+	 * Main purpose of MachineMove, passes the current gameState to Machine to think on then calls setMove.
+	 */
 	public void play() {
-		// TODO Auto-generated method stub
+		this.moveIndex = Game.getGameState();
+		this.setMove(mech.think(moveIndex));
 
 	}
 
