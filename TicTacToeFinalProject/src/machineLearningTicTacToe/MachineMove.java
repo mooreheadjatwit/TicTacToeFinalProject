b886cc4ecd1028d0f27d26961a8391e0dc679baf
moynihanm1 <moynihanm1@BY1928A75002102.wit.private>
2019-11-14 16:24:19 -0500

    Adding machineMOve

---
+++ TicTacToeFinalProject/src/machineLearningTicTacToe/MachineMove.java
@@ -0,0 +1,17 @@
+package machineLearningTicTacToe;
+
+public class MachineMove implements iPlayer {
+
+	@Override
+	public void setMove(int move) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void play() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
