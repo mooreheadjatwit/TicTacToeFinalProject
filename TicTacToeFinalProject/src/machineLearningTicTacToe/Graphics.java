1ea142c84a914edbece94677a66856bf2b53b237
moynihanm1 <moynihanm1@BY1928A75002102.wit.private>
2019-11-21 16:33:58 -0500

    lots or errors coming

--- TicTacToeFinalProject/src/machineLearningTicTacToe/Graphics.java
+++ TicTacToeFinalProject/src/machineLearningTicTacToe/Graphics.java
@@ -30,7 +30,7 @@ public class Graphics extends Application {
 	Scene loss;
 
 	private boolean TurnX = true;
-	private int[] array = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+	private int[] moveIndex = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 
 	private Text shape = new Text();
 
@@ -94,7 +94,7 @@ public class Graphics extends Application {
 	}
 	
 	
-	///////////////////////////////////////////////array return below
+	///////////////////////////////////////////////moveIndex return below
 	public int boardDisplay () {
 		BorderPane secondScene = new BorderPane();
 		game = new Scene(secondScene, 600, 600);
@@ -130,15 +130,15 @@ public class Graphics extends Application {
 		
 		btn1.setOnAction(e -> {
 
-			if (TurnX == true && isFull(array[0]) == true) {
-				array[0] = 1;
+			if (TurnX == true && isFull(moveIndex[0]) == true) {
+				moveIndex[0] = 1;
 				index = 0;
-			//	board.add(xOorblank(array[0]), 0, 0);
+			//	board.add(xOorblank(moveIndex[0]), 0, 0);
 				TurnX = false;
-			} else if(isFull(array[0]) == true){
-				array[0] = 2;
+			} else if(isFull(moveIndex[0]) == true){
+				moveIndex[0] = 2;
 				index = 0;
-			//	board.add(xOorblank(array[0]), 0, 0);
+			//	board.add(xOorblank(moveIndex[0]), 0, 0);
 				TurnX = true;
 			}
 
@@ -148,15 +148,15 @@ public class Graphics extends Application {
 
 		btn2.setOnAction(e -> {
 
-			if (TurnX == true && isFull(array[1]) == true) {
-				array[1] = 1;
+			if (TurnX == true && isFull(moveIndex[1]) == true) {
+				moveIndex[1] = 1;
 				index = 1;
-			//	board.add(xOorblank(array[1]), 1, 0);
+			//	board.add(xOorblank(moveIndex[1]), 1, 0);
 				TurnX = false;
-			} else if( isFull(array[1]) == true){
-				array[1] = 2;
+			} else if( isFull(moveIndex[1]) == true){
+				moveIndex[1] = 2;
 				index = 1;
-			//	board.add(xOorblank(array[1]), 1, 0);
+			//	board.add(xOorblank(moveIndex[1]), 1, 0);
 				TurnX = true;
 			}
 
@@ -164,15 +164,15 @@ public class Graphics extends Application {
 
 		btn3.setOnAction(e -> {
 
-			if (TurnX == true && isFull(array[2]) == true) {
-				array[2] = 1;
+			if (TurnX == true && isFull(moveIndex[2]) == true) {
+				moveIndex[2] = 1;
 				index = 2;
-			//	board.add(xOorblank(array[2]), 2, 0);
+			//	board.add(xOorblank(moveIndex[2]), 2, 0);
 				TurnX = false;
-			} else if(isFull(array[2]) == true){
-				array[2] = 2;
+			} else if(isFull(moveIndex[2]) == true){
+				moveIndex[2] = 2;
 				index = 2;
-			//	board.add(xOorblank(array[2]), 2, 0);
+			//	board.add(xOorblank(moveIndex[2]), 2, 0);
 				TurnX = true;
 			}
 
@@ -180,15 +180,15 @@ public class Graphics extends Application {
 
 		btn4.setOnAction(e -> {
 
-			if (TurnX == true && isFull(array[3]) == true) {
-				array[3] = 1;
+			if (TurnX == true && isFull(moveIndex[3]) == true) {
+				moveIndex[3] = 1;
 				index = 3;
-			//	board.add(xOorblank(array[3]), 0, 1);
+			//	board.add(xOorblank(moveIndex[3]), 0, 1);
 				TurnX = false;
-			} else if(isFull(array[3]) == true){
-				array[3] = 2;
+			} else if(isFull(moveIndex[3]) == true){
+				moveIndex[3] = 2;
 				index = 3;
-			//	board.add(xOorblank(array[3]), 0, 1);
+			//	board.add(xOorblank(moveIndex[3]), 0, 1);
 				TurnX = true;
 			}
 
@@ -196,15 +196,15 @@ public class Graphics extends Application {
 
 		btn5.setOnAction(e -> {
 
-			if (TurnX == true && isFull(array[4]) == true) {
-				array[4] = 1;
+			if (TurnX == true && isFull(moveIndex[4]) == true) {
+				moveIndex[4] = 1;
 				index = 4;
-				//board.add(xOorblank(array[4]), 1, 1);
+				//board.add(xOorblank(moveIndex[4]), 1, 1);
 				TurnX = false;
-			} else if( isFull(array[4]) == true){
-				array[4] = 2;
+			} else if( isFull(moveIndex[4]) == true){
+				moveIndex[4] = 2;
 				index = 4;
-				//board.add(xOorblank(array[4]), 1, 1);
+				//board.add(xOorblank(moveIndex[4]), 1, 1);
 				TurnX = true;
 			}
 
@@ -212,15 +212,15 @@ public class Graphics extends Application {
 
 		btn6.setOnAction(e -> {
 
-			if (TurnX == true && isFull(array[5]) == true) {
-				array[5] = 1;
+			if (TurnX == true && isFull(moveIndex[5]) == true) {
+				moveIndex[5] = 1;
 				index = 5;
-				//board.add(xOorblank(array[5]), 2, 1);
+				//board.add(xOorblank(moveIndex[5]), 2, 1);
 				TurnX = false;
-			} else if(isFull(array[5]) == true){
-				array[5] = 2;
+			} else if(isFull(moveIndex[5]) == true){
+				moveIndex[5] = 2;
 				index = 5;
-				//board.add(xOorblank(array[5]), 2, 1);
+				//board.add(xOorblank(moveIndex[5]), 2, 1);
 				TurnX = true;
 			}
 
@@ -228,16 +228,16 @@ public class Graphics extends Application {
 
 		btn7.setOnAction(e -> {
 
-			if (TurnX == true && isFull(array[6]) == true) {
-				array[6] = 1;
+			if (TurnX == true && isFull(moveIndex[6]) == true) {
+				moveIndex[6] = 1;
 				index = 6;
-				//board.add(xOorblank(array[6]), 0, 2);
+				//board.add(xOorblank(moveIndex[6]), 0, 2);
 				TurnX = false;
 				
-			} else if(isFull(array[6]) == true){
-				array[6] = 2;
+			} else if(isFull(moveIndex[6]) == true){
+				moveIndex[6] = 2;
 				index = 6;
-				//board.add(xOorblank(array[6]), 0, 2);
+				//board.add(xOorblank(moveIndex[6]), 0, 2);
 				TurnX = true;
 			}
 
@@ -245,15 +245,15 @@ public class Graphics extends Application {
 
 		btn8.setOnAction(e -> {
 
-			if (TurnX == true && isFull(array[7]) == true) {
-				array[7] = 1;
+			if (TurnX == true && isFull(moveIndex[7]) == true) {
+				moveIndex[7] = 1;
 				index = 7;
-				//board.add(xOorblank(array[7]), 1, 2);
+				//board.add(xOorblank(moveIndex[7]), 1, 2);
 				TurnX = false;
-			} else if( isFull(array[7]) == true){
-				array[7] = 2;
+			} else if( isFull(moveIndex[7]) == true){
+				moveIndex[7] = 2;
 				index = 7;
-				//board.add(xOorblank(array[7]), 1, 2);
+				//board.add(xOorblank(moveIndex[7]), 1, 2);
 				TurnX = true;
 			}
 
@@ -261,15 +261,15 @@ public class Graphics extends Application {
 
 		btn9.setOnAction(e -> {
 
-			if (TurnX == true && isFull(array[8]) == true) {
-				array[8] =1;
+			if (TurnX == true && isFull(moveIndex[8]) == true) {
+				moveIndex[8] =1;
 				index = 8;
-				//board.add(xOorblank(array[8]), 2, 2);
+				//board.add(xOorblank(moveIndex[8]), 2, 2);
 				TurnX = false;
-			} else if( isFull(array[8]) == true){
-				array[8] = 2;
+			} else if( isFull(moveIndex[8]) == true){
+				moveIndex[8] = 2;
 				index = 8;
-				//board.add(xOorblank(array[8]), 2, 2);
+				//board.add(xOorblank(moveIndex[8]), 2, 2);
 				TurnX = true;
 			}
 			
@@ -493,6 +493,7 @@ loss = new Scene(victoryScreen,600,600);
 		//////////////////////
 		//TEMP PRINT METHOD 
 		//////////////////////
+		/*
 		@SuppressWarnings("resource")
 		Scanner s = new Scanner(System.in);
 		Game.printGameState();
@@ -507,9 +508,238 @@ loss = new Scene(victoryScreen,600,600);
 				System.out.println("Please enter an availible index [0-8]");
 			}
 		}
+		*/
 		///////////////////////////
 		//END TEMP PRINT METHOD
 		///////////////////////////
-		return move;
+		BorderPane secondScene = new BorderPane();
+		Scene game = new Scene(secondScene, 600, 600);
+		
+		GridPane controls = new GridPane();
+		GridPane.setConstraints(controls, 2, 2);
+		controls.setAlignment(Pos.BASELINE_CENTER);
+
+		final Button btn1 = new Button("Pos UL");
+		final Button btn2 = new Button("Pos UM");
+		final Button btn3 = new Button("Pos UR");
+
+		final Button btn4 = new Button("Pos ML");
+		final Button btn5 = new Button("Pos MM");
+		final Button btn6 = new Button("Pos MR");
+
+		final Button btn7 = new Button("Pos BL");
+		final Button btn8 = new Button("Pos BM");
+		final Button btn9 = new Button("Pos BR");
+
+		controls.add(btn1, 0, 0);
+		controls.add(btn2, 1, 0);
+		controls.add(btn3, 2, 0);
+		controls.add(btn4, 0, 1);
+		controls.add(btn5, 1, 1);
+		controls.add(btn6, 2, 1);
+		controls.add(btn7, 0, 2);
+		controls.add(btn8, 1, 2);
+		controls.add(btn9, 2, 2);
+		secondScene.setLeft(controls);
+		
+		
+		int index = 0;
+		btn1.setOnAction(e -> {
+
+			if (moveIndex[0] == 0) {
+				index = 0;
+			} else {
+
+			}
+
+			
+			
+		});
+
+		btn2.setOnAction(e -> {
+
+			if (TurnX == true && isFull(moveIndex[1]) == true) {
+				moveIndex[1] = 1;
+				index = 1;
+			//	board.add(xOorblank(moveIndex[1]), 1, 0);
+				TurnX = false;
+			} else if( isFull(moveIndex[1]) == true){
+				moveIndex[1] = 2;
+				index = 1;
+			//	board.add(xOorblank(moveIndex[1]), 1, 0);
+				TurnX = true;
+			}
+
+		});
+
+		btn3.setOnAction(e -> {
+
+			if (TurnX == true && isFull(moveIndex[2]) == true) {
+				moveIndex[2] = 1;
+				index = 2;
+			//	board.add(xOorblank(moveIndex[2]), 2, 0);
+				TurnX = false;
+			} else if(isFull(moveIndex[2]) == true){
+				moveIndex[2] = 2;
+				index = 2;
+			//	board.add(xOorblank(moveIndex[2]), 2, 0);
+				TurnX = true;
+			}
+
+		});
+
+		btn4.setOnAction(e -> {
+
+			if (TurnX == true && isFull(moveIndex[3]) == true) {
+				moveIndex[3] = 1;
+				index = 3;
+			//	board.add(xOorblank(moveIndex[3]), 0, 1);
+				TurnX = false;
+			} else if(isFull(moveIndex[3]) == true){
+				moveIndex[3] = 2;
+				index = 3;
+			//	board.add(xOorblank(moveIndex[3]), 0, 1);
+				TurnX = true;
+			}
+
+		});
+
+		btn5.setOnAction(e -> {
+
+			if (TurnX == true && isFull(moveIndex[4]) == true) {
+				moveIndex[4] = 1;
+				index = 4;
+				//board.add(xOorblank(moveIndex[4]), 1, 1);
+				TurnX = false;
+			} else if( isFull(moveIndex[4]) == true){
+				moveIndex[4] = 2;
+				index = 4;
+				//board.add(xOorblank(moveIndex[4]), 1, 1);
+				TurnX = true;
+			}
+
+		});
+
+		btn6.setOnAction(e -> {
+
+			if (TurnX == true && isFull(moveIndex[5]) == true) {
+				moveIndex[5] = 1;
+				index = 5;
+				//board.add(xOorblank(moveIndex[5]), 2, 1);
+				TurnX = false;
+			} else if(isFull(moveIndex[5]) == true){
+				moveIndex[5] = 2;
+				index = 5;
+				//board.add(xOorblank(moveIndex[5]), 2, 1);
+				TurnX = true;
+			}
+
+		});
+
+		btn7.setOnAction(e -> {
+
+			if (TurnX == true && isFull(moveIndex[6]) == true) {
+				moveIndex[6] = 1;
+				index = 6;
+				//board.add(xOorblank(moveIndex[6]), 0, 2);
+				TurnX = false;
+				
+			} else if(isFull(moveIndex[6]) == true){
+				moveIndex[6] = 2;
+				index = 6;
+				//board.add(xOorblank(moveIndex[6]), 0, 2);
+				TurnX = true;
+			}
+
+		});
+
+		btn8.setOnAction(e -> {
+
+			if (TurnX == true && isFull(moveIndex[7]) == true) {
+				moveIndex[7] = 1;
+				index = 7;
+				//board.add(xOorblank(moveIndex[7]), 1, 2);
+				TurnX = false;
+			} else if( isFull(moveIndex[7]) == true){
+				moveIndex[7] = 2;
+				index = 7;
+				//board.add(xOorblank(moveIndex[7]), 1, 2);
+				TurnX = true;
+			}
+
+		});
+
+		btn9.setOnAction(e -> {
+
+			if (TurnX == true && isFull(moveIndex[8]) == true) {
+				moveIndex[8] =1;
+				index = 8;
+				//board.add(xOorblank(moveIndex[8]), 2, 2);
+				TurnX = false;
+			} else if( isFull(moveIndex[8]) == true){
+				moveIndex[8] = 2;
+				index = 8;
+				//board.add(xOorblank(moveIndex[8]), 2, 2);
+				TurnX = true;
+			}
+			
+		});
+
+		
+		GridPane board = new GridPane();
+		GridPane.setConstraints(board, 2, 2);
+
+		Rectangle border1 = new Rectangle(200, 200);
+		border1.setFill(null);
+		border1.setStroke(Color.BLACK);
+
+		Rectangle border2 = new Rectangle(200, 200);
+		border2.setFill(null);
+		border2.setStroke(Color.BLACK);
+
+		Rectangle border3 = new Rectangle(200, 200);
+		border3.setFill(null);
+		border3.setStroke(Color.BLACK);
+
+		Rectangle border4 = new Rectangle(200, 200);
+		border4.setFill(null);
+		border4.setStroke(Color.BLACK);
+
+		Rectangle border5 = new Rectangle(200, 200);
+		border5.setFill(null);
+		border5.setStroke(Color.BLACK);
+
+		Rectangle border6 = new Rectangle(200, 200);
+		border6.setFill(null);
+		border6.setStroke(Color.BLACK);
+
+		Rectangle border7 = new Rectangle(200, 200);
+		border7.setFill(null);
+		border7.setStroke(Color.BLACK);
+
+		Rectangle border8 = new Rectangle(200, 200);
+		border8.setFill(null);
+		border8.setStroke(Color.BLACK);
+
+		Rectangle border9 = new Rectangle(200, 200);
+		border9.setFill(null);
+		border9.setStroke(Color.BLACK);
+
+		board.add(border1, 0, 0);
+		board.add(border2, 1, 0);
+		board.add(border3, 2, 0);
+		board.add(border4, 0, 1);
+		board.add(border5, 1, 1);
+		board.add(border6, 2, 1);
+		board.add(border7, 0, 2);
+		board.add(border8, 1, 2);
+		board.add(border9, 2, 2);
+
+		secondScene.setCenter(board);
+		
+		return index;	
+		
+		
+	
 	}
 }
