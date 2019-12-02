c85faff85211c97cfe09e3cfd488f2ce68d7360e
moynihanm1 <moynihanm1@BY1928A75002102.wit.private>
2019-12-02 16:55:50 -0500

    slight update to lonely learning

--- TicTacToeFinalProject/src/machineLearningTicTacToe/LonelyLearning.java
+++ TicTacToeFinalProject/src/machineLearningTicTacToe/LonelyLearning.java
@@ -9,7 +9,7 @@ public class LonelyLearning {
 		int tie = 0;
 		int one = 0;
 		int two = 0;
-		int numGames = 100;
+		int numGames = 1000;
 		Machine test = new Machine(2);
 		int[] array = new int[] {2,0,0,1,1,2,0,0,1};
 		//System.out.println(Mutator.trinary(array));
